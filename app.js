1f76bdd9394c3dc86273366a4849333d004fb6e6
Ismoilov Dilshodbek <idilshodbek23@gmail.com>
2022-10-13 21:25:35 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/',(req,res,next)=>{
+    res.send("This is working")
+})
+
+app.listen(port,()=>{
+    console.log(`Application is running on ${port}`);
+})
